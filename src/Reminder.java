dc72df2d1a689467a314d920b2d22edd4d88b31c
HunterMetcalf6273 <80177041+HunterMetcalf6273@users.noreply.github.com>
2021-04-10 13:19:17 -0700

    Create Reminder.java

---
+++ src/Reminder.java
@@ -0,0 +1,9 @@
+import Java.util.Date;
+interface Reminder{
+    //Return the name of the reminder, like "Roses" or "Dog"
+    String getNameString();
+    //Return what is actually displayed in the calendar, like "Water roses" or "Feed dog"
+    String getVerbString();
+    //Return an array of Date objects between the start date and the end date, also given as Date objects
+    Date[] getDates(Date start, Date end);
+}
