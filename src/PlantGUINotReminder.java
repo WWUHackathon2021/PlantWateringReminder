c7907490afae0e1cead761cf313f66e4bffdbcb4
Michizane Ohata <ohatam@wwu.edu>
2021-04-10 22:51:39 -0700

    Update PlantGUINotReminder.java
    More descriptions  in comments, remember to do setSize AFTER setting up the buttons because it will only overlap the buttons for some weird reason

--- src/PlantGUINotReminder.java
+++ src/PlantGUINotReminder.java
@@ -1,4 +1,7 @@
 // The main reason this is a main fucntion is because its mostly to experiment and learn about the swing library. 
+// 
+// Helpful link to get the boxes to click, particularly the last one to switch between different tabs like view, my plants, add plant:
+// https://www.guru99.com/java-swing-gui.html (This program does not have code implemented for the tabs yet)
 import javax.swing.*;///
 import java.awt.*;
 
@@ -13,7 +16,7 @@ class gui{
        button2.setBounds(230,100,100, 40);//x axis, y axis, width, height
        frame.getContentPane().add(button1); // Adds Button to content pane of frame
        frame.getContentPane().add(button2); // Adds Button to content pane of frame
-       frame.setSize(800,800);
+       frame.setSize(800,800); // Sets the size of the used window
        
        frame.setLayout(null);
        frame.setVisible(true);
